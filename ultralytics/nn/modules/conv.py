f246cf74428c7f0deb12047daaffb3bc1d37376e
Michał Bilski <michubilu@wp.pl>
2025-04-01 21:37:48 +0200

    ECA improvement

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -714,26 +714,18 @@ class Index(nn.Module):
         return x[self.index]
 
 class ECAAttention(nn.Module):
-    """Constructs a ECA module.
-    Args:
-        channel: Number of channels of the input feature map
-        k_size: Adaptive selection of kernel size
-    """
-
-    def __init__(self, c1, k_size=3):
+    def __init__(self, channels, kernel_size=3):
         super(ECAAttention, self).__init__()
+        self.kernel_size = kernel_size
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
+        self.conv = nn.Conv2d(channels, channels, kernel_size=kernel_size, padding=(kernel_size - 1) // 2, bias=False)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        # feature descriptor on the global spatial information
-        y = self.avg_pool(x)
-        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
-        # Multi-scale information fusion
-        y = self.sigmoid(y)
-
-        return x * y.expand_as(x)
+        y = self.avg_pool(x)  # Global Average Pooling
+        y = self.conv(y)  # 1D Convolution
+        y = self.sigmoid(y)  # Sigmoid activation
+        return x * y
 
 class ELA(nn.Module):
     """Constructs an Efficient Local Attention module.
